a4d05e6889b4dfc7938d04f33f6dbf1cb58c95db
Kurt McAlpine <kurt@linux.com>
2017-04-08 18:54:23 +1200

    Get depenencies

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -4,5 +4,6 @@ ARTIFACT=$1
 mkdir -p /go/src/github.com/ci-pipeline/
 cp -r cloudformation-resource /go/src/github.com/ci-pipeline/
 cd /go/src/github.com/ci-pipeline/cloudformation-resource/${ARTIFACT}
+go get
 go build
 cp ${ARTIFACT} ${BUILD_DIR}/${ARTIFACT}/${ARTIFACT}
