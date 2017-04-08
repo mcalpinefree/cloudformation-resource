7497d183a8365919c70e1bd13c78be5cd243214f
Kurt McAlpine <kurt@linux.com>
2017-04-09 00:23:43 +1200

    Added test step

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 export CGO_ENABLED=0
 BUILD_DIR=$(pwd)
 ARTIFACT=$1
@@ -6,5 +7,6 @@ mkdir -p /go/src/github.com/ci-pipeline/
 cp -r cloudformation-resource /go/src/github.com/ci-pipeline/
 cd /go/src/github.com/ci-pipeline/cloudformation-resource/${ARTIFACT}
 go get
+go test
 go build
 cp ${ARTIFACT} ${BUILD_DIR}/${ARTIFACT}/${ARTIFACT}
